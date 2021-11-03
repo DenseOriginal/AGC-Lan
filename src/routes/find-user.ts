301dfabccab118355ca748ced986f2f3fb7aa270
Anders <ak.kornerup@gmail.com>
2021-11-03 21:10:27 +0100

    Redirect user

--- src/routes/find-user.ts
+++ src/routes/find-user.ts
@@ -5,6 +5,9 @@ export const getUser: RequestHandler = async (req, res) => {
   // Retrieve the id from the url /user/:id
   const id = req.params.id;
 
+  // If the user is trying to view themself, redirect them to /profile
+  if(id == req.user?._id) return res.redirect("/profile");
+
   try {
     const foundUser = (await UserModel.findOne({ _id: id }).exec())?.toObject();
 
