8dd929bfceaf59cc584a2aa6f239448188531b6f
Anders <ak.kornerup@gmail.com>
2021-11-03 14:48:55 +0100

    Add user to render

--- src/routes/admin/admin.ts
+++ src/routes/admin/admin.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
 
 export const getAdmin: RequestHandler = (req, res) => {
-  res.render('admin', { title: "Admin" });
+  res.render('admin', { user: req.user, title: "Admin" });
 }
