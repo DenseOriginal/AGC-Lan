a7207a86a2b206df34fba1c815d9f2277e5c460b
Anders <ak.kornerup@gmail.com>
2021-11-05 21:35:23 +0100

    LAN mongoose schema

---
+++ src/models/lan.ts
@@ -0,0 +1,52 @@
+import { Model, model, Schema } from "mongoose";
+
+interface ILAN {
+  name: string;
+  description: string;
+  start: Date;
+  end: Date;
+  created_at: Date;
+  updated_at: Date;
+  public: boolean;
+  registration_open: boolean;
+  price: number;
+}
+
+export type LANAsDocument = ILAN & Document;
+
+const LANSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  start: { type: Date, required: true },
+  end: { type: Date, required: true },
+  created_at: { type: Date, default: new Date, immutable: true },
+  last_updated: { type: Date, default: new Date, required: true },
+  public: { type: Boolean, default: false, },
+  registration_open: { type: Boolean, default: false },
+  price: { type: Number, default: 0 },
+  // TODO: Implement seats
+  // TODO: Implement LAN User
+  // TODO: Lan image
+});
+
+/**
+ * Add your
+ * - pre-save hooks
+ * - validations
+ * - victuals
+ */
+ LANSchema.pre('save', async function(this: LANAsDocument, next) {
+   this.updated_at = new Date();
+ });
+
+ /**
+  * Methods
+  */
+ LANSchema.method({});
+ 
+ /**
+  * Statics
+  */
+ LANSchema.statics = {};
+ 
+ export const PartialUserModel: Model<ILAN> = model('PartialUsers', LANSchema);
