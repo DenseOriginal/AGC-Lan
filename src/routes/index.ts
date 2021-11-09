75209150ae8b62cd765a255f5e175fcdddd36d69
Anders <ak.kornerup@gmail.com>
2021-11-09 12:42:38 +0100

    CoOoOoOol new flex-column class

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import { RequestHandler } from "express";
+import { getNextLanAsObject } from "./lan/helper";
 
-export const getIndex: RequestHandler = (req, res) => {
-  res.status(200).render('index', { user: req.user });
+export const getIndex: RequestHandler = async (req, res) => {
+  res.status(200).render('index', {
+    user: req.user,
+    nextLan: await getNextLanAsObject(),
+  });
 }
