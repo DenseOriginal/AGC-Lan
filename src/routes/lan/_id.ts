ea1c00a00148000ece16dc66c062bd8789a1f38f
Anders <ak.kornerup@gmail.com>
2021-11-10 18:29:03 +0100

    Fixed old implementation

--- src/routes/lan/_id.ts
+++ src/routes/lan/_id.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { isValidObjectId } from "mongoose";
+import { isStaff } from "../../config/passport";
 import { LanModel } from "../../models/lan";
 
 export const getLan: RequestHandler = async (req, res) => {
@@ -19,7 +20,7 @@ export const getLan: RequestHandler = async (req, res) => {
     // If no lan was found
     // or if the lan isn't public and the user isn't a staff
     // Then tell the user that the lan doesn't exist
-    if((!foundLan) || (!foundLan.public && !req.user?.is_staff)) return res.render("lan/_id", {
+    if((!foundLan) || (!foundLan.public && !isStaff(req.user))) return res.render("lan/_id", {
       user: req.user,
       title: 'No lan',
       error: 'Dette LAN findes ikke'
