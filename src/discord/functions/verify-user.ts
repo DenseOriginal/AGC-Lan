d3b7db4e2ccd713ab10523ffc0b25cc00aecb5bb
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-11-20 21:59:39 +0100

    Better way of giving verified role

--- src/discord/functions/verify-user.ts
+++ src/discord/functions/verify-user.ts
@@ -4,7 +4,5 @@ import { environment } from "../environment";
 export async function verifyUser(userId: string) {
   const guild = await client.guilds.fetch(environment.guildId);
   const user = await guild.members.fetch(userId);
-  const verifiedRole = await guild.roles.fetch(environment.verifiedRoleId);
-  if(!verifiedRole) throw new Error("Can't find Verified role");
-  await user.roles.add(verifiedRole);
+  user.roles.add(environment.verifiedRoleId);
 }
