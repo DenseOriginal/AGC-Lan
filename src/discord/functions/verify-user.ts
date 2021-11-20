3d3811d7cc2d011fcc179c069d079c3fc3af2346
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-11-20 21:54:48 +0100

    Created environment variables for discord

--- src/discord/functions/verify-user.ts
+++ src/discord/functions/verify-user.ts
@@ -1,9 +1,10 @@
 import { client } from "../client";
+import { environment } from "../environment";
 
 export async function verifyUser(userId: string) {
-  const guild = await client.guilds.fetch(process.env.DISCORD_GUILD_ID as string);
+  const guild = await client.guilds.fetch(environment.guildId);
   const user = await guild.members.fetch(userId);
-  const verifiedRole = await guild.roles.cache.find(role => role.name == "Verified");
+  const verifiedRole = await guild.roles.fetch(environment.verifiedRoleId);
   if(!verifiedRole) throw new Error("Can't find Verified role");
   await user.roles.add(verifiedRole);
 }
