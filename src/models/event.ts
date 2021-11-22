3f83dfcb03ec2fd881f72a54c0b850b1ff51096f
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-11-22 09:55:06 +0100

    Created event model

---
+++ src/models/event.ts
@@ -0,0 +1,42 @@
+import { Model, model, Schema, Document } from "mongoose";
+
+export interface IEvent {
+  title: string;
+  description: string;
+  start: Date;
+  end: Date;
+  created_at: Date;
+  public: boolean;
+}
+
+export type EventAsDocument = IEvent & Document;
+
+const Event = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, default: '', required: false },
+  start: { type: Date, required: true },
+  end: { type: Date, required: true },
+  created_at: { type: Date, default: new Date, immutable: true },
+  public: { type: Boolean, required: true },
+});
+
+/**
+ * Add your
+ * - pre-save hooks
+ * - validations
+ * - victuals
+ */
+ Event.pre('save', async function(this: EventAsDocument, next) {
+ });
+
+ /**
+  * Methods
+  */
+ Event.method({});
+ 
+ /**
+  * Statics
+  */
+ Event.statics = {};
+ 
+ export const EventModel: Model<IEvent> = model('Event', Event);
