a27eea2072682acaa363782daf9745caec354fb6
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-11-22 09:16:20 +0100

    Added lan event source

--- src/routes/api/calendar.ts
+++ src/routes/api/calendar.ts
@@ -1,1 +1,31 @@
-export const t = 1
+import { RequestHandler } from "express";
+import { LanModel } from "../../models/lan";
+
+interface CalendarEvent {
+  title: string;
+  start: string;
+  end: string;
+}
+
+// Help https://fullcalendar.io/docs/events-json-feed
+
+export const getLanEventSource: RequestHandler = async (req, res) => {
+  // The request params can contain a start time and an end time
+  // The query should only contain stuff between the start and end
+  const { start, end } = req.params;
+
+  const allLan = await LanModel.find({
+    start: {
+      $gte: new Date(start || 0), // If we have a start time pass that in, otherwise set it to 0
+      $lte: new Date(end || Date.now() * 2) // If twe have an end time use that, otherwise just pick a time lon in the future
+    },
+    public: true
+  }).exec();
+
+  res.send(JSON.stringify(
+    allLan
+      // Add one day to the end to make the calendar display it correctly
+      .map(lan => { lan.end = new Date(lan.end.getTime() + 86400000 /* One day */); return lan })
+      .map(lan => ({ title: lan.name, start: lan.start, end: lan.end, allDay: true }))
+  ));
+} 
--- src/routes/api/router.ts
+++ src/routes/api/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
+import { getLanEventSource } from "./calendar";
 import { callback } from "./callback";
 
 export const ApiRouter = Router();
@@ -7,5 +8,8 @@ export const ApiRouter = Router();
 // Routes used for discord authentication
 ApiRouter.route('/login').get(passport.authenticate('discord'));
 ApiRouter.route('/callback').get(passport.authenticate('discord'), callback);
+
+// Calendar routes
+ApiRouter.route('/calendar/lan').get(getLanEventSource);
 
 // Other api routes
