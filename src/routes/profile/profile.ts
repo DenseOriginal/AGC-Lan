414aad9f4353f200ab9343427403d6a1d29c461f
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-11-25 22:18:12 +0100

    Allow user to update their profile

--- src/routes/profile/profile.ts
+++ src/routes/profile/profile.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from "crypto";
 import { RequestHandler } from "express";
+import { UserModel } from "../../models/user";
 
 export const getProfile: RequestHandler = (req, res) => {
   // Generate a csrf token, which is just a random string
@@ -13,3 +14,43 @@ export const getProfile: RequestHandler = (req, res) => {
   // Send something to the user
   res.status(200).render('profile/profile', { user: req.user, title: "Profile", csrfToken });
 };
+
+// Regex to validate a klass
+// 20HTXCR : Pass
+// 2HTXCR : Fail
+// 20CR : Fail
+const classRegex = /\d\dHTX\w{1,4}/;
+
+export const postProfile: RequestHandler = async (req, res) => {
+  // Get the posted date from the client
+  const { first_name, last_name, class: klasse, username } = req.body as { [index: string]: string | undefined };
+
+  // The only data we really need to validate it the class, to make sure it fits the correct way of writing
+  // because the others are just strings, that are free
+  // Therefore we can just do one if statement
+  // And if the failes, just do a silent error (This is bad)
+  // TODO: Better errorhandling
+  if(classRegex.test((klasse || "").toUpperCase())) {
+    try {
+      const newUser = await UserModel.findByIdAndUpdate(req.user?._id, {
+        first_name: first_name,
+        last_name: last_name,
+        class: klasse,
+        username: username,
+      }).exec();
+
+      req.user = newUser?.toObject();
+    } catch (error) {
+      return res.render('error', { error: 'Der er sket en uventet fejl' });
+    }
+  }
+
+  // Janky way of removing the #edit from the profile url, because the hash is stored client side
+  // We have no way of removing it, so instead just pass a variable to the handlebars template
+  // An tell it to render javascript that removes the hash if this is set to true...
+  res.locals.removeEditHash = true;
+  
+  // We can't just redirect to the /profile so just pass the req and res objetc to the function
+  // This is also very janky but locals don't persist through redirects, and using session is way overkill
+  getProfile(req, res, () => {});
+}
--- src/routes/profile/router.ts
+++ src/routes/profile/router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { isAuthenticated, isAwaitingSetup } from "../../config/guards";
 import { getDelete } from "./delete";
-import { getProfile } from "./profile";
+import { getProfile, postProfile } from "./profile";
 import { getSetup, postSetup } from "./setup";
 
 export const ProfileRouter = Router();
 
 // Profile routes
 ProfileRouter.route('/profile').get(isAuthenticated, getProfile);
+ProfileRouter.route('/profile').post(isAuthenticated, postProfile);
 ProfileRouter.route('/profile/setup').get(isAwaitingSetup, getSetup);
 ProfileRouter.route('/profile/setup').post(isAwaitingSetup, postSetup);
 ProfileRouter.route('/profile/delete').post(isAuthenticated, getDelete);
