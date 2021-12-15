3abfeb5a779327c143bc13ac8275e0ded4d62ff6
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2021-12-15 09:30:23 +0100

    Added under construction to /turneringer

--- src/routes/tournaments/router.ts
+++ src/routes/tournaments/router.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { getConstruction } from "../construction";
 import { getTournamentList } from "./list";
 
 export const TournamentsRouter = Router();
+
+// If we're not in development, serve a under construction for all requests
+// To /turneringer
+// TODO: Fix /turneringer route, and remove this
+if (process.env.NODE_ENV !== "development") TournamentsRouter.route('**').get(getConstruction);
 
 TournamentsRouter.route("/").get(getTournamentList);
