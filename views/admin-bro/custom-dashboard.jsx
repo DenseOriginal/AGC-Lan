bc048ca0d74cab8d98fea366821dbb0ba326e6df
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-21 09:51:47 +0100

    Added link back from /staff

--- views/admin-bro/custom-dashboard.jsx
+++ views/admin-bro/custom-dashboard.jsx
@@ -1,7 +1,9 @@
 // This is only so that we can remove default dashboard
 // Don't worry there isn't going to be any tsx bullshit in this repo
 const Dashboard = () => {
-  return ('Hello there');
+  return (
+    <a href="/">Tilbage til aglan.dk</a>
+  );
 }
 
 export default Dashboard
