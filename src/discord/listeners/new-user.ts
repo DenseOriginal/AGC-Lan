621373d8f0d58cd043e61dcfebec6bbc9f59e9fe
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-22 22:02:16 +0100

    Rewrote new-user.ts

--- src/discord/listeners/new-user.ts
+++ src/discord/listeners/new-user.ts
@@ -1,18 +1,14 @@
-import { ArgsOf, Client, Discord, On } from "discordx";
+import { GuildMember } from "discord.js";
 import { UserModel } from "../../models/user";
+import { client } from "../client";
 import { environment } from "../environment";
 
-@Discord()
-class OnNewUser {
-  @On('guildMemberAdd')
-  private async newUser(
-    [member]: ArgsOf<"guildMemberAdd">,
-  ) {
-    try {
-      const dbUser = await UserModel.findOne({ discord_id: member.id }).exec();
-      if(dbUser) member.roles.add(environment.verifiedRoleId);
-    } catch (error) {
-      console.error(error);
-    }
+
+client.on('guildMemberAdd', async (member: GuildMember) => {
+  try {
+    const dbUser = await UserModel.findOne({ discord_id: member.id }).exec();
+    if(dbUser) member.roles.add(environment.verifiedRoleId);
+  } catch (error) {
+    console.error(error);
   }
-}
+});
