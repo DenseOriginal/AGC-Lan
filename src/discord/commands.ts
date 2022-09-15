0f47c4928ead9eda1399ff0cfdf215807fb951ce
Anders Kok <ak.kornerup@gmail.com>
2022-09-15 21:27:42 +0200

    Discord: Remove programmaticly settings slash command perms

--- src/discord/commands.ts
+++ src/discord/commands.ts
@@ -35,30 +35,6 @@ export async function setupSlashCommands() {
   commands.forEach(cmd => {
     commandCollection.set(cmd.name, cmd);
   });
-
-  // Setup permissions
-
-  // Get all the guildCommand id's
-  // guild.commands.fetch() doesn't work, and results a wierd error
-  // Se we can use rest instead
-  // We can also infer our own type, but we only need the id and name
-  const commandsFromAPI = await rest.get(Routes.applicationGuildCommands(environment.clientId, environment.guildId)) as { id: string, name: string }[];
-
-  // Loop through all fetched commands, and set permissions
-  for (const { id, name } of commandsFromAPI) {
-    // Get the command from the collection
-    const command = commandCollection.get(name);
-
-    if(!command) continue; // If the command doesn't exist, then continue
-    if(!command.permissions) continue; // If the command doesn't have ane restrictions, then continue
-
-    // discordjs seems broken, so we need to use raw api
-    // https://discord.com/developers/docs/interactions/application-commands#get-guild-application-command-permissions
-    await rest.put(
-      Routes.applicationCommandPermissions(environment.clientId, environment.guildId, id),
-      { body: { permissions: command.permissions } }
-    )
-  }
 }
 
 export async function handleInteractions(interaction: Interaction) {
